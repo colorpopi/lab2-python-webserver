b4a7accf50f4096478442eb0f1685023c4f682b4
Diane <colorpop@Dianes-MBP.fios-router.home>
2022-02-17 23:01:07 -0500

    changed msg back

--- solution.py
+++ solution.py
@@ -27,7 +27,7 @@ def webServer(port=13331):
         
         #Send one HTTP header line into socket.
         #Fill in start
-        connectionSocket.send("OK".encode())
+        connectionSocket.send("200 OK".encode())
         #Fill in end
 
         #Send the content of the requested file to the client
