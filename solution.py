6feeea0e4c823cb0a478ebaaf6863409e567d249
Diane <colorpop@Dianes-MBP.fios-router.home>
2022-02-17 22:43:51 -0500

    encoding messages

--- solution.py
+++ solution.py
@@ -27,7 +27,7 @@ def webServer(port=13331):
         
         #Send one HTTP header line into socket.
         #Fill in start
-        connectionSocket.send("\nHTTP/ 200 OK")
+        connectionSocket.send("\nHTTP/ 200 OK").encode()
         #Fill in end
 
         #Send the content of the requested file to the client
@@ -39,7 +39,7 @@ def webServer(port=13331):
       except IOError:
         # Send response message for file not found (404)
         #Fill in start
-        connectionSocket.send("\nHTTP/ 404 Not Found")
+        connectionSocket.send("\nHTTP/ 404 Not Found").encode()
         #Fill in end
 
         #Close client socket
