b302be2f322c598a2a85230ef450d70a0524e1d3
Diane <colorpop@Dianes-MBP.fios-router.home>
2022-02-17 22:20:28 -0500

    edits

--- solution.py
+++ solution.py
@@ -8,25 +8,22 @@ def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
     # Prepare a server socket
     serverSocket.bind(("", port))
-
     serverSocket.listen(1)
 
     while True:
         # Establish the connection
         # print('Ready to serve...')
-        # print("Connection open..")
         connectionSocket, addr = serverSocket.accept()
         try:
 
             try:
-                # can receive up to 1024 bytes
-                message = serverSocket.recv(1024)
+                message = serverSocket.recv(2048)
                 filename = message.split()[1]
                 f = open(filename[1:])
-                outputdata = f.read()
+                outputdata = f.read().decode()
 
                 # Send one HTTP header line into socket.
-                connectionSocket.send("200 OK".encode("utf-8"))
+                connectionSocket.send("200 OK".encode())
 
                 # Send the content of the requested file to the client
                 for i in range(0, len(outputdata)):
@@ -36,7 +33,7 @@ def webServer(port=13331):
                 connectionSocket.close()
             except IOError:
                 # Send response message for file not found (404)
-                connectionSocket.send("404 Not Found".encode("utf-8"))
+                connectionSocket.send("404 Not Found".encode("))
                 # Close client socket
                 connectionSocket.close()
 
